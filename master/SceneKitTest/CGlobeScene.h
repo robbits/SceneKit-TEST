b6c0aa76e7808de6c08add7225506f284881432c
Rob James <rob@robfjames.com>
2015-03-12 21:37:25 -0400

    build

---
+++ master/SceneKitTest/CGlobeScene.h
@@ -0,0 +1,12 @@
+//
+//  CGlobeScene.h
+//  TwitterSceneKitTest
+//
+//  Created by Rob James on 8/16/13.
+//
+
+#import <SceneKit/SceneKit.h>
+
+@interface CGlobeScene : SCNScene
+
+@end
---
+++ master/SceneKitTest/GameViewController.swift
@@ -0,0 +1,110 @@
+//
+//  GameViewController.swift
+//  SceneKitTest
+//
+//  Created by Rob James on 6/6/14.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = GlobeScene()
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light?.type = SCNLightTypeOmni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+
+        // create and add an ambient light to the scene
+//        let ambientLightNode = SCNNode()
+//        ambientLightNode.light = SCNLight()
+//        ambientLightNode.light.type = SCNLightTypeAmbient
+//        ambientLightNode.light.color = UIColor.darkGrayColor()
+//        scene.rootNode.addChildNode(ambientLightNode)
+
+        // retrieve the SCNView
+        let scnView = self.view as SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.blackColor()
+        
+        // add a tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
+        let gestureRecognizers = NSMutableArray()
+        gestureRecognizers.addObject(tapGesture)
+        if let arr = scnView.gestureRecognizers { gestureRecognizers.addObjectsFromArray(arr) }
+        scnView.gestureRecognizers = gestureRecognizers
+    }
+    
+    func handleTap(gestureRecognize: UIGestureRecognizer) {
+        // retrieve the SCNView
+        let scnView = self.view as SCNView
+        
+        // check what nodes are tapped
+        let p = gestureRecognize.locationInView(scnView)
+        let hitResults = scnView.hitTest(p, options: nil)
+        
+        // check that we clicked on at least one object
+        if hitResults?.count > 0 {
+            // retrieved the first clicked object
+            let result: AnyObject! = hitResults?[0]
+            
+            // get its material
+            let material = result.node!.geometry?.firstMaterial
+            
+            // highlight it
+            SCNTransaction.begin()
+            SCNTransaction.setAnimationDuration(0.5)
+            
+            // on completion - unhighlight
+            SCNTransaction.setCompletionBlock {
+                SCNTransaction.begin()
+                SCNTransaction.setAnimationDuration(0.5)
+                
+                material?.emission.contents = UIColor.blackColor()
+                
+                SCNTransaction.commit()
+            }
+            
+            material?.emission.contents = UIColor.redColor()
+            
+            SCNTransaction.commit()
+        }
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
